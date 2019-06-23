8e1dc17ee592892ffbbeb371f6b20b00aba0a175
ADARSH MAMODIA <adarshmamodia26@gmail.com>
2019-06-23 17:21:26 +0530

    Changes in PES-HTTPSByDefault

--- cypress/integration/PES/PES-HTTPSByDefault.js
+++ cypress/integration/PES/PES-HTTPSByDefault.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 describe('PES-HTTPSByDefault-Content-Verification', function(){
-
+/*
 before(function() {
      cy.log('runs once before all tests in the block')
      
@@ -16,7 +16,7 @@ before(function() {
      cy.wait(50000)
      })
      
-})
+})*/
 
 it('Verify that HTTPSByDefault is implemented',function(){
 //reading Symbol values of HTTPSByDefault
